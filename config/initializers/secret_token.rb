a162d89f00e824fa591596493466552138d46dc0
Nicolas Molina <nicolasmolina@Nicolass-MacBook-Pro.local>
2013-03-18 08:31:35 +0200

    1stcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Andydick::Application.config.secret_token = '48870186698e5214b6fad0cac2684140a4ed16d9895076c892dbc0b6409afc513722b2e07ebfe4ad12e12474e697a72365cd7950f63d3af42c4149e5822332df'
